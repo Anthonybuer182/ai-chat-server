5d2ad65e67fdee160a63b3625d8fdde1998ee774
weiqun <1348172474@qq.com>
2024-12-05 17:49:07 +0800

    鉴权和数据库代码完成

--- src/http/auth2.py
+++ src/http/auth2.py
@@ -31,7 +31,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
         expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
+    return encoded_jwt,(expire - datetime.utcnow()).total_seconds()
 
 def decode_access_token(token: str):
     try:
--- src/http/router_user.py
+++ src/http/router_user.py
@@ -13,24 +13,18 @@ router = APIRouter()
 
 @router.post("/token")
 async def token(request: OAuth2PasswordRequestForm = Depends(),response_model=BaseResponse[Token],db: AsyncSession = Depends(get_db)):
-    user = get_user(db,request.username)
+    user = await get_user(db,request.username)
     if not user or user["password"] != request.password:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    auth2.create_access_token({"sub": request.username})
-    # access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    # access_token = jwt.encode(
-    #     {"sub": form_data.username, "exp": datetime.utcnow() + access_token_expires},
-    #     SECRET_KEY,
-    #     algorithm=ALGORITHM,
-    # )
-    return success_response(data=Token(access_token=request.username, token_type=request.password,expires_in=200))
+    access_token,expire=auth2.create_access_token(data={"sub": request.username})
+    return success_response(data=Token(access_token=access_token, token_type="bearer",expires_in=expire))
 @router.post("/register")
 async def register(request:Register,response_model=BaseResponse[Token],db: AsyncSession = Depends(get_db)):
-    user=create_user(db, request.username, request.password, request.phone, request.email)
-    token_response = await token(request=OAuth2PasswordRequestForm(username=request.username, password=request.password))
+    user=await create_user(db, request.username, request.password, request.phone, request.email)
+    token_response = await token(request=OAuth2PasswordRequestForm(username=user.username, password=user.password))
     return token_response
 

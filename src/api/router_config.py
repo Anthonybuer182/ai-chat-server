fb650e5f45569c3aee3a6735ce797b8bd22105b6
weiqun <1348172474@qq.com>
2025-01-20 15:47:56 +0800

    聊天消息接口

--- src/api/router_config.py
+++ src/api/router_config.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
-from src.api import router_auth2, router_character, router_session, router_generate, router_user
+from src.api import router_auth2, router_character, router_session, router_message, router_generate, router_user
 restful_router = APIRouter()
 restful_router.include_router(router_auth2.router, tags=["Token"])
 restful_router.include_router(router_user.router, prefix="/user", tags=["User"])
 restful_router.include_router(router_character.router, prefix="/character", tags=["Character"])
 restful_router.include_router(router_session.router, prefix="/session", tags=["Session"])
 restful_router.include_router(router_generate.router, prefix="/generate", tags=["Generate"])
+restful_router.include_router(router_message.router, prefix="/message", tags=["Message"])
 

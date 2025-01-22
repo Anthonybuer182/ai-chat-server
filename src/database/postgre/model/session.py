94e92b9907244526747b15fa2743fa0b4a9eb510
weiqun <1348172474@qq.com>
2025-01-22 17:40:19 +0800

    优化

--- src/database/postgre/model/session.py
+++ src/database/postgre/model/session.py
@@ -9,8 +9,8 @@ class SessionDB(BaseDB):
     user_id = Column(UUID(as_uuid=True), ForeignKey("users.id", ondelete="CASCADE"), index=True, nullable=False, doc="关联的用户ID")
     user_name = Column(String(64), index=True, nullable=False, doc="用户名称")
     character_id = Column(UUID(as_uuid=True), ForeignKey("characters.id", ondelete="CASCADE"), index=True, nullable=False, doc="角色ID")
-    new_message = Column(Text, nullable=False, doc="最新消息")
-    messages_context = Column(JSON, nullable=False, doc="历史消息上下文（JSON 格式）")
+    new_message = Column(Text, nullable=True, doc="最新消息")
+    messages_context = Column(JSON, nullable=True, doc="历史消息上下文（JSON 格式）")
 
     __table_args__ = (
         Index('ix_user_character', "user_id", "character_id"),
